e7f91f4fb14b6ef5f8976a69c7fd071f4c11afec
Tushar Panchal <panchalt597@gmail.com>
2025-06-23 22:02:19 +0530

    Initial commit

---
+++ creat_Array.py
@@ -0,0 +1,54 @@
+# ARRAY CREATING
+
+import numpy as np
+
+# ----------------------1st Array == ( 1-D )-------------------------------------------------------
+
+# x=[1,2,3,4,5]
+# y = np.array(x) # type 1
+# y1 = np.array([1,2,3,4,5]) #type 2
+#
+# print(y,y1)
+# print("",type(y),"\n",type(y1),"\n",type(x),"\n")
+#
+# l =[]
+#
+# for i in range(1,5):
+#     INT_1 = int(input("Enter a Number:-"))
+#     l.append(INT_1)
+# print(np.array(l))
+# print(y.ndim,y1.ndim)
+
+#----------------------------- 2nd Array == ( 2-D ) -----------------------------------------------
+
+# ar2 = np.array([[1,2,3,4,5],[1,2,3,4,5]])
+# print(ar2)
+# print(ar2.ndim)
+
+#----------------------------- 3nd Array == ( 3-D ) -----------------------------------------------
+
+# ar3 = np.array([[[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]])
+# print(ar3)
+# print(ar3.ndim)
+
+#----------------------------- nth Array == ( N-D ) -----------------------------------------------
+
+# arn = np.array([1,2,3], ndmin =2)
+# print(arn)
+# print(arn.ndim)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
